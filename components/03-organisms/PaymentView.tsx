59daa569e141ae33eaa153b05d7f50a4e48d1718
Naw Singh <naw_123@hotmail.nl>
2022-02-22 21:44:21 +0100

    Stripe payment now returns to correct url

--- components/03-organisms/PaymentView.tsx
+++ components/03-organisms/PaymentView.tsx
@@ -153,7 +153,7 @@ export default function ViewPayment({
             name: accountHolderName,
           },
         },
-        return_url: `http://localhost:3000/${router.locale}/completed?orderId=${orderId}&recordId=${orderDataRes.data.id}`,
+        return_url: `${process.env.NEXT_PUBLIC_BASE_URL}/${router.locale}/completed?orderId=${orderId}&recordId=${orderDataRes.data.id}`,
       });
       setLoading(true);
       if (error) {
