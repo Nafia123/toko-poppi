4af35dd6ef2f80a838b4ca2412ae43d17324f51a
Naw Singh <naw_123@hotmail.nl>
2022-03-06 16:20:11 +0100

    Animation is a bit smoother

--- pages/completed/index.tsx
+++ pages/completed/index.tsx
@@ -70,7 +70,7 @@ export default function completed() {
             <div className="col-start-2 col-span-4 md:col-start-2 md:col-end-3 flex px-10 md:px-5  md:h-40 align-middle">
               <p className="text-5xl text-center font-bold text-gray-600 my-auto">
                 <LoaderFailed completeLoader={startAnimation} />
-                {t('completeOrder.failMessage')}
+               {startAnimation ? <p className="transition-opacity">{t('completeOrder.failMessage')}</p> : ''}
               </p>
             </div>
           </div>
@@ -79,7 +79,7 @@ export default function completed() {
             <div className="col-start-2 col-span-4 md:col-start-2 md:col-end-3 flex px-10 md:px-5  md:h-40 align-middle">
               <p className="text-5xl text-center font-bold text-gray-600 my-auto">
                 <LoaderComplete completeLoader={startAnimation} />
-                {t('completeOrder.confirmationMessage')}
+                {startAnimation ? <p className="transition-opacity">{t('completeOrder.confirmationMessage')}</p> : ''}
               </p>
             </div>
           </div>
